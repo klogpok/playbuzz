7917b53803b36389b46d722111cb2dcd12295f31
klogpok <kloleg@gmail.com>
2019-07-10 18:50:54 +0300

    Updates

---
+++ precache-manifest.0692e39f19a487abd54dc0da296abafc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3fa255f0c803f5f3e8bb1f4e8b0c181",
+    "url": "/playbuzz/index.html"
+  },
+  {
+    "revision": "1c906434cc93af6d90c3",
+    "url": "/playbuzz/static/css/main.8c37c653.chunk.css"
+  },
+  {
+    "revision": "8dcb5eef0d57d323d222",
+    "url": "/playbuzz/static/js/2.04ed7b43.chunk.js"
+  },
+  {
+    "revision": "1c906434cc93af6d90c3",
+    "url": "/playbuzz/static/js/main.5a2d67ec.chunk.js"
+  },
+  {
+    "revision": "714030b5f6973d379519",
+    "url": "/playbuzz/static/js/runtime~main.5ab3009e.js"
+  }
+]);
